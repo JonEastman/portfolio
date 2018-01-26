780e8a67296ca3adc664c881ed03f3016997149c
Jon Eastman <eastman.jonathan.d@gmail.com>
2018-01-25 18:50:36 -0600

    scroll spy added - general clean up

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,5 +4,20 @@ var $ = require('jquery');
 
 $(document).ready(function() {
 
+    // $(window).scroll(function() {
+    //     var scroll = $(window).scrollTop();
+    //
+    //     if (scroll >= 500) {
+    //         $('#js-nav-background').addClass('');
+    //     } else {
+    //
+    //     }
+    // });
 
+    $(function () {
+        $(document).scroll(function () {
+            var $nav = $(".navbar-fixed-top");
+            $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+        });
+    });
 });
